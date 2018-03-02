594bdcdabb2446880cd37ceab531915aac7c8c02
zcadditions <raymondabarbour@gmail.com>
2018-03-02 16:54:32 -0500

    Create template_info.php

---
+++ includes-templates-bootstrap/template_info.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Template Information File
+ * 
+ * BOOTSTRAP v1.0.BETA
+ *
+ * @package templateSystem
+ * @copyright Copyright 2003-2005 Zen Cart Development Team
+ * @copyright Portions Copyright 2003 osCommerce
+ * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
+ * @version $Id: template_info.php 2306 2005-11-05 21:34:28Z wilt $
+ */
+$template_name = 'ZCA Bootstrap';
+$template_version = 'Version 1.0.BETA';
+$template_author = 'Raymond A. Barbour';
+$template_description = 'Responsive Zen Cart Template built with Bootstrap 4';
+$template_screenshot = '';
+?>
