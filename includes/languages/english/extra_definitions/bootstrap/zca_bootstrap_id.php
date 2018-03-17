7239dd24aee8ffde61894f131e2c0c2a0384411c
lat9 <lat9@vinosdefrutastropicales.com>
2018-03-17 13:57:22 -0400

    #17: Enable template-cloning
    An additional file (/includes/languages/english/extra_definitions/bootstrap/zca_bootstrap_id.php) is added.  That file contains a single definition, the presence of which identifies the template as bootstrap or a clone.

---
+++ includes/languages/english/extra_definitions/bootstrap/zca_bootstrap_id.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * 
+ * zca_bootstrap_id.php
+ *
+ * @copyright Copyright 2018 zcadditions.com/vinosdefrutastropicales.com
+ * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
+ */
+// -----
+// The presence of the following definition identifies the template as either
+// the ZCA "Bootstrap" template, or a clone thereof.  See GitHub#17 in the
+// template's GitHub repository for additional information.
+//
+// Note: It doesn't matter what value is set, just that the definition exists
+// for the template's zca_bootstrap_active function's use.  That function will
+// identify the current template as a "bootstrap" one, if the definition is
+// present.
+//
+define('IS_ZCA_BOOTSTRAP_TEMPLATE', 'true');
