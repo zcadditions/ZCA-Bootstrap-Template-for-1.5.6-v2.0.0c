7217d41a3267887f33682498b0edefce73d5b9ca
zcadditions <raymondabarbour@gmail.com>
2018-04-24 12:29:17 -0400

    #42 rename bootstrap example elements

--- includes/templates/bootstrap/modalboxes/tpl_cvv_help.php
+++ includes/templates/bootstrap/modalboxes/tpl_cvv_help.php
@@ -16,7 +16,7 @@
   <div class="modal-dialog" role="document">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel"><?php echo HEADING_CVV; ?></h5>
+        <h5 class="modal-title" id="cvvHelpModalLabel"><?php echo HEADING_CVV; ?></h5>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
