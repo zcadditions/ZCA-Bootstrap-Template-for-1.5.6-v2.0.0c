8f76cebe7d2508f67477a4ba548e07abef8517db
zcadditions <raymondabarbour@gmail.com>
2018-04-24 12:27:28 -0400

    #42 rename bootstrap example elements

--- includes/templates/bootstrap/modalboxes/tpl_bootstrap_images.php
+++ includes/templates/bootstrap/modalboxes/tpl_bootstrap_images.php
@@ -12,7 +12,7 @@
 ?>
 <!-- Modal -->
 <!-- BOOTSTRAP -->
-<div class="modal fade bd-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+<div class="modal fade bootstrap-slide-modal-lg" tabindex="-1" role="dialog" aria-labelledby="bootStrapImagesModalLabel" aria-hidden="true">
   <div class="modal-dialog modal-lg">
           <div class="modal-content">
       <div class="modal-header">
