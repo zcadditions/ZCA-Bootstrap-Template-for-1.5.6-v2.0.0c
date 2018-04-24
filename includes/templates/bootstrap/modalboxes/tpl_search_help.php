afdc4a7402e35077521d154dca2225f6d9043e87
zcadditions <raymondabarbour@gmail.com>
2018-04-24 12:32:18 -0400

    #42 rename bootstrap example elements

--- includes/templates/bootstrap/modalboxes/tpl_search_help.php
+++ includes/templates/bootstrap/modalboxes/tpl_search_help.php
@@ -12,11 +12,11 @@
 ?>
 
 <!-- Modal -->
-<div class="modal fade" id="searchHelpModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+<div class="modal fade" id="searchHelpModal" tabindex="-1" role="dialog" aria-labelledby="searchHelpModalLabel" aria-hidden="true">
   <div class="modal-dialog" role="document">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel"><?php echo TEXT_SEARCH_HELP_LINK; ?></h5>
+        <h5 class="modal-title" id="searchHelpModalLabel"><?php echo TEXT_SEARCH_HELP_LINK; ?></h5>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
