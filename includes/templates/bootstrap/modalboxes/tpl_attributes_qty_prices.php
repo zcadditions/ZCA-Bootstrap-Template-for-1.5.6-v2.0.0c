4a887338001f0b934c118e6be854c69da41c954b
zcadditions <raymondabarbour@gmail.com>
2018-04-24 12:28:08 -0400

    #42 rename bootstrap example elements

--- includes/templates/bootstrap/modalboxes/tpl_attributes_qty_prices.php
+++ includes/templates/bootstrap/modalboxes/tpl_attributes_qty_prices.php
@@ -17,7 +17,7 @@
   <div class="modal-dialog" role="document">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel"><?php echo TEXT_ATTRIBUTES_QTY_PRICES_HELP; ?></h5>
+        <h5 class="modal-title" id="attributesQtyPricesModalLabel"><?php echo TEXT_ATTRIBUTES_QTY_PRICES_HELP; ?></h5>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
