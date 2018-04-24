575fb0cf3a2c8c8d4c3e45a58477bba332250d64
zcadditions <raymondabarbour@gmail.com>
2018-04-24 12:31:40 -0400

    #42 rename bootstrap example elements

--- includes/templates/bootstrap/modalboxes/tpl_info_shopping_cart.php
+++ includes/templates/bootstrap/modalboxes/tpl_info_shopping_cart.php
@@ -5,7 +5,7 @@
   <div class="modal-dialog" role="document">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel"><?php echo HEADING_TITLE_MODAL; ?></h5>
+        <h5 class="modal-title" id="cartHelpModalLabel"><?php echo HEADING_TITLE_MODAL; ?></h5>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
