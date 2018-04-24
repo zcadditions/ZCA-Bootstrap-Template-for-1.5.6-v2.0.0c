6d0e389bf56d4c8caead331777c6dd8d4a2ef563
zcadditions <raymondabarbour@gmail.com>
2018-04-24 12:30:10 -0400

    #42 rename bootstrap example elements

--- includes/templates/bootstrap/modalboxes/tpl_image.php
+++ includes/templates/bootstrap/modalboxes/tpl_image.php
@@ -15,7 +15,7 @@
   <div class="modal-dialog modal-lg">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel"><?php echo $products_name; ?></h5>
+        <h5 class="modal-title" id="myLargeModalLabel"><?php echo $products_name; ?></h5>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
