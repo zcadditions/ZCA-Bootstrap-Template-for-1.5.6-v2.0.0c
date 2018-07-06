8edf63839e8047efd19973b0a90fe90e3d943802
zcadditions <raymondabarbour@gmail.com>
2018-07-06 11:22:31 -0400

    #65 admin controlled colors

---
+++ YOUR_ADMIN/includes/languages/english/extra_definitions/zca_bootstrap_colors.php
@@ -0,0 +1,13 @@
+<?php
+/* 
+ *
+ *  zca_bootstrap_colors.php
+ *
+ * @package zencart_auto_installer
+ * @copyright Copyright 2003-2015 ZenCart.Codes a Pro-Webs Company
+ * @copyright Copyright 2003-2015 Zen Cart Development Team
+ * @copyright Portions Copyright 2003 osCommerce
+ * @version Author: rbarbour (ZCAdditions.com)  7/01/2018 06:00 AM Modified ZCA-BS-COLORS
+ */
+
+define('BOX_TOOLS_ZCA_BOOTSTRAP_COLORS', "ZCA Bootstrap Colors");
