899b02f05aa265d1c1f7090898c04f7e9ba0c440
zcadditions <raymondabarbour@gmail.com>
2018-07-06 12:40:10 -0400

    #64 Photoswipe

---
+++ includes/classes/zca_photoswipe/jquery_photoswipe.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ *
+ * @copyright Copyright 2003-2005 Zen Cart Development Team
+ * @copyright Portions Copyright 2003 osCommerce
+ * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
+ */
+
+echo '<link rel="stylesheet prefetch" href="https://cdnjs.cloudflare.com/ajax/libs/photoswipe/4.1.1/photoswipe.min.css">';
+echo '<link rel="stylesheet prefetch" href="https://cdnjs.cloudflare.com/ajax/libs/photoswipe/4.1.1/default-skin/default-skin.min.css">';
+echo '<script src="https://cdnjs.cloudflare.com/ajax/libs/photoswipe/4.1.1/photoswipe.min.js"></script>';
+echo '<script src="https://cdnjs.cloudflare.com/ajax/libs/photoswipe/4.1.1/photoswipe-ui-default.min.js"></script>';
+?>
+
+<script language="javascript" type="text/javascript">
