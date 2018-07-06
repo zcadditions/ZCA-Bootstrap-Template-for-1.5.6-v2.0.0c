3596fe6de3a96d195719dc688857e50a2ae70dfe
zcadditions <raymondabarbour@gmail.com>
2018-07-06 12:53:31 -0400

    #64 Photoswipe

---
+++ includes/modules/pages/product_reviews/jscript_zca_photoswipe.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ *
+ * @copyright Copyright 2003-2005 Zen Cart Development Team
+ * @copyright Portions Copyright 2003 osCommerce
+ * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
+ */
+
+	require_once(DIR_FS_CATALOG . DIR_WS_CLASSES . 'zca_photoswipe/jquery_photoswipe.php');
+ 	require_once(DIR_FS_CATALOG . DIR_WS_CLASSES . 'zca_photoswipe/autoload_default.php');
+
+?>
