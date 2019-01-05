412b7897c1c7fbb3aaadf06add75905aab9b8fdc
zcadditions <raymondabarbour@gmail.com>
2019-01-05 13:21:24 -0500

    #91 issue update

--- includes/modules/centerboxes/specials_index.php
+++ includes/modules/centerboxes/specials_index.php
@@ -1,37 +1,35 @@
 <?php
 /**
- * specials_products module
- * 
- * BOOTSTRAP v1.0.BETA
+ * specials_index module
  *
  * @package modules
- * @copyright Copyright 2003-2007 Zen Cart Development Team
+ * @copyright Copyright 2003-2018 Zen Cart Development Team
  * @copyright Portions Copyright 2003 osCommerce
  * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
- * @version $Id: specials_products.php 6424 2007-05-31 05:59:21Z ajeh $
+ * @version $Id: mc12345678 Wed Jan 24 21:14:33 2018 -0500 Modified in v1.5.6 $
  */
 if (!defined('IS_ADMIN_FLAG')) {
   die('Illegal Access');
 }
 
 // initialize vars
-$categories_products_id_list = '';
+$categories_products_id_list = array();
 $list_of_products = '';
-$specials_products_query = '';
+$specials_index_query = '';
 $display_limit = '';
 
-if ( (($manufacturers_id > 0 && $_GET['filter_id'] == 0) || $_GET['music_genre_id'] > 0 || $_GET['record_company_id'] > 0) || (!isset($new_products_category_id) || $new_products_category_id == '0') ) {
-  $specials_products_query = "select p.products_id, p.products_image, pd.products_name, p.master_categories_id
-                           from (" . TABLE_PRODUCTS . " p
-                           left join " . TABLE_SPECIALS . " s on p.products_id = s.products_id
-                           left join " . TABLE_PRODUCTS_DESCRIPTION . " pd on p.products_id = pd.products_id )
-                           where p.products_id = s.products_id
-                           and p.products_id = pd.products_id
-                           and p.products_status = '1' and s.status = 1
-                           and pd.language_id = '" . (int)$_SESSION['languages_id'] . "'";
+if ( (($manufacturers_id > 0 && empty($_GET['filter_id'])) || !empty($_GET['music_genre_id']) || !empty($_GET['record_company_id'])) || empty($new_products_category_id) ) {
+  $specials_index_query = "SELECT p.products_id, p.products_image, pd.products_name, p.master_categories_id
+                           FROM (" . TABLE_PRODUCTS . " p
+                           LEFT JOIN " . TABLE_SPECIALS . " s ON p.products_id = s.products_id
+                           LEFT JOIN " . TABLE_PRODUCTS_DESCRIPTION . " pd ON p.products_id = pd.products_id )
+                           WHERE p.products_id = s.products_id
+                           AND p.products_id = pd.products_id
+                           AND p.products_status = 1 AND s.status = 1
+                           AND pd.language_id = " . (int)$_SESSION['languages_id'];
 } else {
   // get all products and cPaths in this subcat tree
-  $productsInCategory = zen_get_categories_products_list( (($manufacturers_id > 0 && $_GET['filter_id'] > 0) ? zen_get_generated_category_path_rev($_GET['filter_id']) : $cPath), false, true, 0, $display_limit);
+  $productsInCategory = zen_get_categories_products_list( (($manufacturers_id > 0 && !empty($_GET['filter_id'])) ? zen_get_generated_category_path_rev($_GET['filter_id']) : $cPath), false, true, 0, $display_limit);
 
   if (is_array($productsInCategory) && sizeof($productsInCategory) > 0) {
     // build products-list string to insert into SQL query
@@ -39,25 +37,25 @@ if ( (($manufacturers_id > 0 && $_GET['filter_id'] == 0) || $_GET['music_genre_i
       $list_of_products .= $key . ', ';
     }
     $list_of_products = substr($list_of_products, 0, -2); // remove trailing comma
-    $specials_products_query = "select distinct p.products_id, p.products_image, pd.products_name, p.master_categories_id
-                             from (" . TABLE_PRODUCTS . " p
-                             left join " . TABLE_SPECIALS . " s on p.products_id = s.products_id
-                             left join " . TABLE_PRODUCTS_DESCRIPTION . " pd on p.products_id = pd.products_id )
-                             where p.products_id = s.products_id
-                             and p.products_id = pd.products_id
-                             and p.products_status = '1' and s.status = '1'
-                             and pd.language_id = '" . (int)$_SESSION['languages_id'] . "'
-                             and p.products_id in (" . $list_of_products . ")";
+    $specials_index_query = "SELECT DISTINCT p.products_id, p.products_image, pd.products_name, p.master_categories_id
+                             FROM (" . TABLE_PRODUCTS . " p
+                             LEFT JOIN " . TABLE_SPECIALS . " s ON p.products_id = s.products_id
+                             LEFT JOIN " . TABLE_PRODUCTS_DESCRIPTION . " pd ON p.products_id = pd.products_id )
+                             WHERE p.products_id = s.products_id
+                             AND p.products_id = pd.products_id
+                             AND p.products_status = 1 AND s.status = 1
+                             AND pd.language_id = " . (int)$_SESSION['languages_id'] . "
+                             AND p.products_id in (" . $list_of_products . ")";
   }
 }
-if ($specials_products_query != '') $specials_products = $db->ExecuteRandomMulti($specials_products_query, MAX_DISPLAY_SPECIAL_PRODUCTS_INDEX);
+if ($specials_index_query != '') $specials_index = $db->ExecuteRandomMulti($specials_index_query, MAX_DISPLAY_SPECIAL_PRODUCTS_INDEX);
 
 $row = 0;
 $col = 0;
 $list_box_contents = array();
 $title = '';
 
-$num_products_count = ($specials_products_query == '') ? 0 : $specials_products->RecordCount();
+$num_products_count = ($specials_index_query == '') ? 0 : $specials_index->RecordCount();
 
 // show only when 1 or more
 if ($num_products_count > 0) {
@@ -68,44 +66,24 @@ if ($num_products_count > 0) {
   }
 
   $list_box_contents = array();
-  while (!$specials_products->EOF) {
+  while (!$specials_index->EOF) {
+    $products_price = zen_get_products_display_price($specials_index->fields['products_id']);
+    if (!isset($productsInCategory[$specials_index->fields['products_id']])) $productsInCategory[$specials_index->fields['products_id']] = zen_get_generated_category_path_rev($specials_index->fields['master_categories_id']);
 
-    if (!isset($productsInCategory[$specials_products->fields['products_id']])) $productsInCategory[$specials_products->fields['products_id']] = zen_get_generated_category_path_rev($specials_products->fields['master_categories_id']);
-    
-/** bof products price */
-    $products_price = zen_get_products_display_price($specials_products->fields['products_id']);
-
-$specials_products_price = '<div class="centerBoxContentsItem-price text-center">' . $products_price . '</div>';
-/** eof products price */
-
-/** bof products name */
-    $specials_products->fields['products_name'] = zen_get_products_name($specials_products->fields['products_id']);   
-    
-$specials_products_name = '<div class="centerBoxContentsItem-name text-center"><a href="' . zen_href_link(zen_get_info_page($specials_products->fields['products_id']), 'cPath=' . $productsInCategory[$specials_products->fields['products_id']] . '&products_id=' . $specials_products->fields['products_id']) . '">' . $specials_products->fields['products_name'] . '</a></div>';
-/** eof products name */
-
-/** bof products image */
-if ($specials_products->fields['products_image'] == '' and PRODUCTS_IMAGE_NO_IMAGE_STATUS == 0) {
-$specials_products_image = '';
-} else {
-$specials_products_image = '<div class="centerBoxContentsItem-image text-center"><a href="' . zen_href_link(zen_get_info_page($specials_products->fields['products_id']), 'cPath=' . $productsInCategory[$specials_products->fields['products_id']] . '&products_id=' . $specials_products->fields['products_id']) . '">' . zen_image(DIR_WS_IMAGES . $specials_products->fields['products_image'], $specials_products->fields['products_name'], SMALL_IMAGE_WIDTH, SMALL_IMAGE_HEIGHT) . '</a></div>';
-}
-/** eof products image */  
-    
-    $list_box_contents[$row][$col] = array('params' => 'class="centerBoxContents card mb-3 p-3 text-center"',
-    'text' => $specials_products_image . $specials_products_name . $specials_products_price);
+    $specials_index->fields['products_name'] = zen_get_products_name($specials_index->fields['products_id']);
+    $list_box_contents[$row][$col] = array('params' => 'class="centerBoxContents card mb-3 p-3 text-center" id="centerBoxContentsSpecials"',
+    'text' => (($specials_index->fields['products_image'] == '' and PRODUCTS_IMAGE_NO_IMAGE_STATUS == 0) ? '' : '<a href="' . zen_href_link(zen_get_info_page($specials_index->fields['products_id']), 'cPath=' . $productsInCategory[$specials_index->fields['products_id']] . '&products_id=' . (int)$specials_index->fields['products_id']) . '">' . zen_image(DIR_WS_IMAGES . $specials_index->fields['products_image'], $specials_index->fields['products_name'], SMALL_IMAGE_WIDTH, SMALL_IMAGE_HEIGHT) . '</a><br />') . '<a href="' . zen_href_link(zen_get_info_page($specials_index->fields['products_id']), 'cPath=' . $productsInCategory[$specials_index->fields['products_id']] . '&products_id=' . $specials_index->fields['products_id']) . '">' . $specials_index->fields['products_name'] . '</a><br />' . $products_price);
 
     $col ++;
     if ($col > (SHOW_PRODUCT_INFO_COLUMNS_SPECIALS_PRODUCTS - 1)) {
       $col = 0;
       $row ++;
     }
-    $specials_products->MoveNextRandom();
+    $specials_index->MoveNextRandom();
   }
 
-  if ($specials_products->RecordCount() > 0) {
+  if ($specials_index->RecordCount() > 0) {
     $title = '<h4 id="specialsCenterbox-card-header" class="centerBoxHeading card-header">' . sprintf(TABLE_HEADING_SPECIALS_INDEX, strftime('%B')) . '</h4>';
     $zc_show_specials = true;
   }
 }
-?>
