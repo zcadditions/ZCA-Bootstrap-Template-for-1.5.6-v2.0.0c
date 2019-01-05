73b73b78e401bb836c5140ba7ac83247df39c238
zcadditions <raymondabarbour@gmail.com>
2019-01-05 14:02:27 -0500

    #119 issue update

--- includes/templates/bootstrap/sideboxes/tpl_information.php
+++ includes/templates/bootstrap/sideboxes/tpl_information.php
@@ -1,21 +1,18 @@
 <?php
 /**
  * Side Box Template
- * 
- * BOOTSTRAP v1.0.BETA
  *
  * @package templateSystem
- * @copyright Copyright 2003-2005 Zen Cart Development Team
+ * @copyright Copyright 2003-2018 Zen Cart Development Team
  * @copyright Portions Copyright 2003 osCommerce
  * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
- * @version $Id: tpl_information.php 2982 2006-02-07 07:56:41Z birdbrain $
+ * @version $Id: Drbyte Sun Jan 7 21:28:50 2018 -0500 Modified in v1.5.6 $
  */
   $content = '';
   $content .= '<div class="list-group-flush sideBoxContent" id="' . str_replace('_', '-', $box_id . 'Content') . '">';
 
-  for ($i=0; $i<sizeof($information); $i++) {
+  for ($i=0, $j=sizeof($information); $i<$j; $i++) {
     $content .= $information[$i] . "\n";
   }
 
   $content .= '</div>';
-?>
