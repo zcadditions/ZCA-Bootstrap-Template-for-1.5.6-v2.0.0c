bde32f5d4973662aa48c830cc04a501fc8ed6a3d
zcadditions <raymondabarbour@gmail.com>
2019-01-05 14:05:15 -0500

    #122 issue update

--- includes/templates/bootstrap/sideboxes/tpl_best_sellers.php
+++ includes/templates/bootstrap/sideboxes/tpl_best_sellers.php
@@ -1,20 +1,17 @@
 <?php
 /**
  * Side Box Template
- * 
- * BOOTSTRAP v1.0.BETA
  *
  * @package templateSystem
- * @copyright Copyright 2003-2005 Zen Cart Development Team
+ * @copyright Copyright 2003-2018 Zen Cart Development Team
  * @copyright Portions Copyright 2003 osCommerce
  * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
- * @version $Id: tpl_best_sellers.php 2982 2006-02-07 07:56:41Z birdbrain $
+ * @version $Id: Drbyte Sun Jan 7 21:28:50 2018 -0500 Modified in v1.5.6 $
  */
   $content = '';
   $content .= '<div id="' . str_replace('_', '-', $box_id . 'Content') . '" class="list-group-flush sideBoxContent">' . "\n";
 
-  for ($i=1; $i<=sizeof($bestsellers_list); $i++) {
+  for ($i=1, $j=sizeof($bestsellers_list); $i<$j; $i++) {
     $content .= '<a class="list-group-item list-group-item-action" href="' . zen_href_link(zen_get_info_page($bestsellers_list[$i]['id']), 'products_id=' . $bestsellers_list[$i]['id']) . '">' . $i . '. ' . zen_trunc_string($bestsellers_list[$i]['name'], BEST_SELLERS_TRUNCATE, BEST_SELLERS_TRUNCATE_MORE) . '</a>';
   }
   $content .= '</div>';
-?>
