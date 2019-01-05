261b78c7e4e9482524b14d3ee1e35c9c0f18f331
zcadditions <raymondabarbour@gmail.com>
2019-01-05 14:01:52 -0500

    #118 issue update

--- includes/templates/bootstrap/sideboxes/tpl_languages.php
+++ includes/templates/bootstrap/sideboxes/tpl_languages.php
@@ -8,11 +8,16 @@
  * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
  * @version $Id: Drbyte Sun Jan 7 21:28:50 2018 -0500 Modified in v1.5.6 $
  */
-  $content = '';
-  $content .= '<div class="list-group-flush sideBoxContent" id="' . str_replace('_', '-', $box_id . 'Content') . '">';
+  $content = "";
+  $content .= '<div id="' . str_replace('_', '-', $box_id . 'Content') . '" class="sideBoxContent text-center">';
 
-  for ($i=0, $j=sizeof($information); $i<$j; $i++) {
-    $content .= $information[$i] . "\n";
+  $lng_cnt = 0;
+  foreach($lng->catalog_languages as $key => $value) {
+    $content .= '<a href="' . zen_href_link($_GET['main_page'], zen_get_all_get_params(array('language', 'currency')) . 'language=' . $key, $request_type) . '">' . zen_image(DIR_WS_LANGUAGES .  $value['directory'] . '/images/' . $value['image'], $value['name']) . '</a>&nbsp;&nbsp;';
+    $lng_cnt ++;
+    if ($lng_cnt >= MAX_LANGUAGE_FLAGS_COLUMNS) {
+      $lng_cnt = 0;
+      $content .= '<br />';
+    }
   }
-
-  $content .= '</div>';
+$content .= '</div>';
