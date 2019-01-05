c8d4bbe397cfd3100f341d5183d9c63e1fecdd9c
zcadditions <raymondabarbour@gmail.com>
2019-01-05 18:43:23 -0500

    #133 issue update

--- includes/templates/bootstrap/templates/tpl_ezpages_bar_header.php
+++ includes/templates/bootstrap/templates/tpl_ezpages_bar_header.php
@@ -10,7 +10,7 @@
  * @copyright Copyright 2003-2006 Zen Cart Development Team
  * @copyright Portions Copyright 2003 osCommerce
  * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
- * @version $Id: tpl_ezpages_bar_header.php 3377 2006-04-05 04:43:11Z ajeh $
+ * @version $Id: DrByte 2019 Jan 04 Modified in v1.5.6a $
  */
 
   /**
@@ -18,7 +18,7 @@
    */
   include(DIR_WS_MODULES . zen_get_module_directory('ezpages_bar_header.php'));
 ?>
-<?php if (sizeof($var_linksList) >= 1) { ?>
+<?php if (!empty($var_linksList)) { ?>
 <div id="ezpagesBarHeader" class="ezpagesBar rounded">
 <ul class="nav nav-pills">
 <?php for ($i=1, $n=sizeof($var_linksList); $i<=$n; $i++) {  ?>
