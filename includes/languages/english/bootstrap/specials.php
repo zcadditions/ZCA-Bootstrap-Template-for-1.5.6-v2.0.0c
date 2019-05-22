ab43d45bcfe6bf4dd902d7273424a71429f005d9
zcadditions <raymondabarbour@gmail.com>
2019-05-22 10:51:21 -0400

    #141 issue

---
+++ includes/languages/english/bootstrap/specials.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * @package languageDefines
+ * @copyright Copyright 2003-2016 Zen Cart Development Team
+ * @copyright Portions Copyright 2003 osCommerce
+ * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
+ * @version $Id: Author: DrByte  Sun Oct 18 01:26:46 2015 -0400 Modified in v1.5.5 $
+ */
+
+  define('NAVBAR_TITLE', 'Specials');
+  define('HEADING_TITLE', 'Specials');
+
+  define('TABLE_HEADING_IMAGE', 'Product Image');
+  define('TABLE_HEADING_MODEL', 'Model');
+  define('TABLE_HEADING_PRODUCTS', 'Product Name');
+  define('TABLE_HEADING_MANUFACTURER', 'Manufacturer');
+  define('TABLE_HEADING_QUANTITY', 'Quantity');
+  define('TABLE_HEADING_PRICE', 'Price');
+  define('TABLE_HEADING_WEIGHT', 'Weight');
+  define('TABLE_HEADING_BUY_NOW', 'Buy Now');
