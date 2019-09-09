773d6ab2b769eaea3bd982df15116236aa416934
Scott C Wilson <scottcwilson@gmail.com>
2019-09-09 13:19:07 -0400

    Default $productArray if not done already

--- includes/modules/pages/shopping_cart/header_php_shopping_cart_zca_bootstrap.php
+++ includes/modules/pages/shopping_cart/header_php_shopping_cart_zca_bootstrap.php
@@ -6,6 +6,7 @@
 // That regex "magic" says find the first '<input type="image"{...}/>', replace it with the
 // button and then copy anything else (like the 'hidden' input that follows).
 //
+if (!isset($productArray)) $productArray = array(); 
 for ($i=0, $n=sizeof((array)$productArray); $i<$n; $i++) {
 $productArray[$i]['buttonUpdate'] = preg_replace('/(<input type="image".*?\/>)(.*)/', '<button type="submit" class="btn"><i class="fas fa-sync-alt"></i></button>$2', $productArray[$i]['buttonUpdate']);  
 }
