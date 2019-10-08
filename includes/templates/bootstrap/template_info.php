149fef3c96468e87f720004e4f1eb430e2d9e856
zcadditions <raymondabarbour@gmail.com>
2019-10-08 12:17:45 -0400

    Update template_info.php

--- includes/templates/bootstrap/template_info.php
+++ includes/templates/bootstrap/template_info.php
@@ -11,7 +11,7 @@
  * @version $Id: template_info.php 2306 2005-11-05 21:34:28Z wilt $
  */
 $template_name = 'ZCA Bootstrap';
-$template_version = 'Version 2.0.0a <a href="' . DIR_WS_CATALOG_TEMPLATE . $template_dir . '/template_readme.html" target = "_blank">Read Me</a>';
+$template_version = 'Version 2.0.0c <a href="' . DIR_WS_CATALOG_TEMPLATE . $template_dir . '/template_readme.html" target = "_blank">Read Me</a>';
 $template_author = 'rbarbour (zcadditions.com)';
 $template_description = 'Responsive Zen Cart Template built with Bootstrap 4';
 $template_screenshot = 'ZCA_BOOTSTRAP_TEMPLATE.png';
