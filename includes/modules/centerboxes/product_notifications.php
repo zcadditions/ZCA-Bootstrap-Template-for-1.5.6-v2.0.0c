f3760d108333e119f4ee7a72cffa2edeb979c0e8
lat9 <cindy@vinosdefrutastropicales.com>
2020-11-03 15:10:40 -0500

    #252:  Products' notification centerbox, refactoring and guest-checkout interoperability.

--- includes/modules/centerboxes/product_notifications.php
+++ includes/modules/centerboxes/product_notifications.php
@@ -2,7 +2,7 @@
 /**
  * product_notifications sidebox - displays a box inviting the customer to sign up for notifications of updates to current product
  *
- * BOOTSTRAP v1.0.BETA
+ * BOOTSTRAP v3.0.0
  * 
  * @package templateSystem
  * @copyright Copyright 2003-2005 Zen Cart Development Team
@@ -12,47 +12,44 @@
  */
 
 // test if box should show
-  $show_product_notifications = false;
+$show_product_notifications = false;
+if (isset($_GET['products_id']) && zen_products_id_valid($_GET['products_id'])) {
+    if (zen_is_logged_in() && !zen_in_guest_checkout()) {
+        $check_query = 
+            "SELECT customers_info_id
+               FROM " . TABLE_CUSTOMERS_INFO . "
+              WHERE customers_info_id = " . (int)$_SESSION['customer_id'] . "
+                AND global_product_notifications = 1
+              LIMIT 1";
+        $check = $db->Execute($check_query);
 
-  if (isset($_GET['products_id']) and zen_products_id_valid($_GET['products_id'])) {
-    if (isset($_SESSION['customer_id'])) {
-      $check_query = "select count(*) as count
-                      from " . TABLE_CUSTOMERS_INFO . "
-                      where customers_info_id = '" . (int)$_SESSION['customer_id'] . "'
-                      and global_product_notifications = '1'";
-
-      $check = $db->Execute($check_query);
-
-      if ($check->fields['count'] <= 0) {
-        $show_product_notifications= true;
-      }
+        if (!$check->EOF) {
+            $show_product_notifications = true;
+        }
     } else {
-      $show_product_notifications= true;
+        $show_product_notifications = true;
     }
-  }
-
-if ($show_product_notifications == true) {
-  if (isset($_GET['products_id'])) {
-    if (isset($_SESSION['customer_id'])) {
-      $check_query = "select count(*) as count
-                      from " . TABLE_PRODUCTS_NOTIFICATIONS . "
-                      where products_id = '" . (int)$_GET['products_id'] . "'
-                      and customers_id = '" . (int)$_SESSION['customer_id'] . "'";
-
-      $check = $db->Execute($check_query);
-
-      $notification_exists = (($check->fields['count'] > 0) ? true : false);
-    } else {
-      $notification_exists = false;
-    }
-
-    if ($notification_exists == true) {
-      require($template->get_template_dir('tpl_modules_yes_notifications.php',DIR_WS_TEMPLATE, $current_page_base,'centerboxes'). '/tpl_modules_yes_notifications.php');
-    } else {
-      require($template->get_template_dir('tpl_modules_no_notifications.php',DIR_WS_TEMPLATE, $current_page_base,'centerboxes'). '/tpl_modules_no_notifications.php');
-    }
-
-
-  }
 }
-?>
+
+if ($show_product_notifications) {
+    if (isset($_GET['products_id'])) {
+        $notification_exists = false;
+        if (zen_is_logged_in() && !zen_in_guest_checkout()) {
+            $check_query = 
+                "SELECT customers_id
+                   FROM " . TABLE_PRODUCTS_NOTIFICATIONS . "
+                  WHERE products_id = " . (int)$_GET['products_id'] . "
+                    AND customers_id = " . (int)$_SESSION['customer_id'] . "
+                  LIMIT 1";
+            $check = $db->Execute($check_query);
+
+            $notification_exists = !$check->EOF;
+        }
+
+        if ($notification_exists) {
+            require $template->get_template_dir('tpl_modules_yes_notifications.php', DIR_WS_TEMPLATE, $current_page_base, 'centerboxes') . '/tpl_modules_yes_notifications.php';
+        } else {
+            require $template->get_template_dir('tpl_modules_no_notifications.php', DIR_WS_TEMPLATE, $current_page_base, 'centerboxes') . '/tpl_modules_no_notifications.php';
+        }
+    }
+}
--- includes/templates/bootstrap/centerboxes/tpl_modules_yes_notifications.php
+++ includes/templates/bootstrap/centerboxes/tpl_modules_yes_notifications.php
@@ -2,7 +2,7 @@
 /**
  * Side Box Template
  * 
- * BOOTSTRAP v1.0.BETA
+ * BOOTSTRAP v3.0.0
  *
  * @package templateSystem
  * @copyright Copyright 2003-2005 Zen Cart Development Team
@@ -10,11 +10,10 @@
  * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
  * @version $Id: tpl_yes_notifications.php 2982 2006-02-07 07:56:41Z birdbrain $
  */
-
-  echo '<div id="productNotifications-centerBoxContents" class="centerBoxContents card mb-3 text-center">';
-  echo '<h4 id="productNotifications-centerBoxHeading" class="centerBoxHeading card-header">' . BOX_HEADING_NOTIFICATIONS . '</h4>';
-  echo '<div id="productNotifications-card-body" class="card-body p-3 text-center">';
-  echo '<a href="' . zen_href_link($_GET['main_page'], zen_get_all_get_params(array('action')) . 'action=notify_remove', $request_type) . '">' . zen_image(DIR_WS_TEMPLATE_IMAGES . OTHER_IMAGE_BOX_NOTIFY_REMOVE, OTHER_BOX_NOTIFY_REMOVE_ALT) . '<br />' . sprintf(BOX_NOTIFICATIONS_NOTIFY_REMOVE, zen_get_products_name($_GET['products_id'])) .'</a>';
-  echo '</div>';
-  echo '</div>';
 ?>
+<div id="ProductNotifications-centerBoxContents" class="centerBoxContents card mb-3 text-center">
+    <h4 id="ProductNotifications-centerBoxHeading" class="centerBoxHeading card-header"><?php echo BOX_HEADING_NOTIFICATIONS; ?></h4>
+    <div id="ProductNotifications-card-body" class="card-body p-3 text-center">
+        <a href="<?php echo zen_href_link($_GET['main_page'], zen_get_all_get_params(array('action')) . 'action=notify', $request_type); ?>"><?php echo zen_image(DIR_WS_TEMPLATE_IMAGES . OTHER_IMAGE_BOX_NOTIFY_REMOVE, OTHER_BOX_NOTIFY_REMOVE_ALT); ?><br><?php echo sprintf(BOX_NOTIFICATIONS_NOTIFY_REMOVE, zen_get_products_name($_GET['products_id'])); ?></a>
+    </div>
+</div>
