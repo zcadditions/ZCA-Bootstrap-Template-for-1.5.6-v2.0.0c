5ea8383964ac65c80ee0b2adcc9ca87d2f00dbfe
lat9 <cindy@vinosdefrutastropicales.com>
2020-11-05 11:32:09 -0500

    #189: Correcting notices on order history sidebox

--- includes/templates/bootstrap/sideboxes/tpl_order_history.php
+++ includes/templates/bootstrap/sideboxes/tpl_order_history.php
@@ -2,21 +2,22 @@
 /**
  * Side Box Template
  *
- * @package templateSystem
- * @copyright Copyright 2003-2018 Zen Cart Development Team
+ * @copyright Copyright 2003-2020 Zen Cart Development Team
  * @copyright Portions Copyright 2003 osCommerce
  * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
- * @version $Id: Drbyte Sun Jan 7 21:28:50 2018 -0500 Modified in v1.5.6 $
+ * @version $Id: DrByte 2020 Jun 19 Modified in v1.5.7 $
  */
-  $content = '';
-  $content .= '<div id="' . str_replace('_', '-', $box_id . 'Content') . '" class="sideBoxContent">' . "\n";
+$content = "";
+$content .= '<div id="' . str_replace('_', '-', $box_id . 'Content') . '" class="sideBoxContent">' . "\n";
 
-  $content .= '<ul class="list-group list-group-flush">';
-  for ($i=1, $j=sizeof($customer_orders); $i<=$j; $i++) {
 
-        
-        $content .= '<li class="list-group-item d-flex justify-content-between align-items-center"><a href="' . zen_href_link(zen_get_info_page($customer_orders[$i]['id']), 'products_id=' . $customer_orders[$i]['id']) . '">' . $customer_orders[$i]['name'] . '</a>&nbsp;&nbsp;<a href="' . zen_href_link($_GET['main_page'], zen_get_all_get_params(array('action')) . 'action=cust_order&pid=' . $customer_orders[$i]['id']) . '"><i class="fas fa-cart-plus"></i></a></li>' . "\n" ;
-        
-  }
-  $content .= '</ul>';
-  $content .= '</div>';
+$content .= '<ul class="list-group list-group-flush">';
+foreach ($customer_orders as $row) {
+    $content .= '<li class="list-group-item d-flex justify-content-between align-items-center">';
+    $content .= '<a href="' . zen_href_link(zen_get_info_page($row['id']), 'products_id=' . $row['id']) . '">' . $row['name'] . '</a>';
+    $content .= '&nbsp;&nbsp;';
+    $content .= '<a href="' . zen_href_link($_GET['main_page'], zen_get_all_get_params(array('action')) . 'action=cust_order&pid=' . $row['id']) . '"><i class="fas fa-cart-plus"></i></a>';
+    $content .= '</li>' . "\n" ;
+}
+$content .= '</ul>' . "\n" ;
+$content .= '</div>';
